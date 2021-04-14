b054da79cb9c8549be4efc0b69ade0f3d8ea784c
agkaw <agkaw@218.236.108.157>
2021-04-14 10:43:52 +0900

    # Person클래스 추가

---
+++ basicgittest/src/com/git/model/vo/Person.java
@@ -0,0 +1,21 @@
+package com.git.model.vo;
+
+public class Person {
+	
+	private String name;
+	private int age;
+	private String email;
+	
+	public Person() {
+		
+	}
+	
+	public Person(String name, int age, String email) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.email = email;
+	}
+	
+	
+}

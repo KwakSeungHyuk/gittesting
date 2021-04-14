80a60d0dc32397f284859be713246517b83b4f55
agkaw <agkaw@218.236.108.157>
2021-04-14 11:12:55 +0900

    #02 toString 메소드 추가 -sh-

--- basicgittest/src/com/git/model/vo/Person.java
+++ basicgittest/src/com/git/model/vo/Person.java
@@ -16,6 +16,35 @@ public class Person {
 		this.age = age;
 		this.email = email;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + ", email=" + email + "]";
+	}
 	
 	
 }
